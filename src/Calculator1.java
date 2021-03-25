14303cbf1ed9036f5339483ae317016fbe8d23e5
Sowndharya <ssowndharya1995@gmail.com>
2021-03-24 20:32:46 -0400

    My second commit

--- src/Calculator1.java
+++ src/Calculator1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Calculator1 {
         public static void main(String[] args) {
             Scanner reader = new Scanner(System.in);
-            System.out.print("Enter two numbers: ");
+            System.out.print(" Please Enter two numbers:  ");
 // nextDouble() reads the next double from the keyboard
             double first = reader.nextDouble();
             double second = reader.nextDouble();
